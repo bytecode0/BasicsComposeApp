ee90d0b7451c731772394967776a38299dce6c9c
Luis Vespa <vespasoft@gmail.com>
2023-11-28 10:01:30 +0100

    AnimatedVisible in OnboardingScreen

--- app/src/main/java/com/mobileinsights/basicscomposeapp/MainActivity.kt
+++ app/src/main/java/com/mobileinsights/basicscomposeapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -14,8 +16,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -42,8 +46,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -51,6 +57,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mobileinsights.basicscomposeapp.ui.theme.BasicsComposeAppTheme
@@ -163,16 +170,45 @@ fun OnboardingScreen(
     modifier: Modifier = Modifier,
     onContinueClicked: () -> Unit
 ) {
-    Column(
+    val animateVisible = remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = true) {
+        animateVisible.value = true
+    }
+    Surface(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        color = MaterialTheme.colorScheme.background
     ) {
-        Text(text = "Welcome to the Basics Compose App")
-        Button(
-            onClick = onContinueClicked
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Continue")
+            AnimatedVisibility(
+                visible = animateVisible.value,
+                enter = fadeIn(animationSpec = tween(2000)),
+                exit = fadeOut(animationSpec = tween(2000))
+            ) {
+                Text(
+                    text = "Welcome to the Basics Compose App",
+                    style = MaterialTheme.typography.headlineMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            AnimatedVisibility(
+                visible = animateVisible.value,
+                enter = fadeIn(animationSpec = tween(4000)),
+                exit = fadeOut(animationSpec = tween(4000))
+            ) {
+                Button(
+                    onClick = onContinueClicked
+                ) {
+                    Text(text = "Continue")
+                }
+            }
+
         }
     }
 }
